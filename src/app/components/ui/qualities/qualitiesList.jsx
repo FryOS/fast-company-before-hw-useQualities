4bf251bc69bceb82a5e7af9b606d2ef43a2de2e7
Alexey Ospishchev <alexeyosp@gmail.com>
2022-09-28 20:50:27 +0300

    Качества отобразили

--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
-import { useQuality } from "../../../hooks/useQuality";
 
 const QualitiesList = ({ qualities }) => {
-    const { isLoading } = useQuality();
-    if (isLoading) return <h1>loading</h1>;
     return (
         <>
-            {qualities.map((qual) => (
-                <Quality key={qual._id} {...qual} />
-            ))}
+            {qualities.map((qual) => {
+                return <Quality key={qual} id={qual} />;
+            })}
         </>
     );
 };
--- src/app/components/ui/qualities/quality.jsx
+++ src/app/components/ui/qualities/quality.jsx
@@ -2,18 +2,15 @@ import PropTypes from "prop-types";
 import React from "react";
 import { useQuality } from "../../../hooks/useQuality";
 
-const Quality = ({ color, name, _id }) => {
+const Quality = ({ id }) => {
     const { isLoading, getQuality } = useQuality();
-    const q = getQuality(_id);
-    console.log(q); // undefined
+    const { color, name } = getQuality(id);
     if (!isLoading) {
         return <span className={"badge m-1 bg-" + color}>{name}</span>;
     } else return "Loading...";
 };
 Quality.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired
 };
 
 export default Quality;
